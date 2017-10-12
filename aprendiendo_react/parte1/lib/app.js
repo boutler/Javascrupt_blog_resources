4bdd6bda3c81486999f1c2a1f0027445730ecb67
Daniel <danielr_soyyo@hotmail.com>
2017-10-12 12:27:22 +0100

    react tutorial files

---
+++ aprendiendo_react/parte1/lib/app.js
@@ -0,0 +1,4 @@
+const template = <h2>¡React funciona!</h2>
+
+
+ReactDOM.render(template, document.getElementById('root'));
---
+++ aprendiendo_react/parte1/public/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var template = React.createElement(
+  'h2',
+  null,
+  '\xA1React funciona! y ¡Babel me observa!'
+);
+
+ReactDOM.render(template, document.getElementById('root'));
+s
